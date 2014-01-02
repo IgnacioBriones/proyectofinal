875b6d303134d0bb92434ad683be9b7120341326
Fernando Rubilar Zepeda <frubilar@icci.cl>
2014-01-02 19:33:14 -0300

    Modelos

--- webinformatica/application/config/config.php
+++ webinformatica/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://146.83.181.4/~frubilar/proyectofinal/webinformatica/index.php/';
 
 /*
 |--------------------------------------------------------------------------
