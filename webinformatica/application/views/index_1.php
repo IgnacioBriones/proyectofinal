f37ff88e1616be08de478abc54eeb97c5e53ed00
Fernando Rubilar Zepeda <frubilar@icci.cl>
2014-01-02 20:03:29 -0300

    Modelos

--- webinformatica/application/views/index_1.php
+++ webinformatica/application/views/index_1.php
@@ -8,15 +8,15 @@
         <link rel="stylesheet" href="<?= base_url() . 'css/reset.css';?>" type="text/css" media="screen">
         <link rel="stylesheet" href="<?= base_url() . 'css/style.css';?>" type="text/css" media="screen">
         <link rel="stylesheet" href="<?= base_url() . 'css/grid.css';?>" type="text/css" media="screen">   
-        <script src="js/jquery-1.6.2.min.js" type="text/javascript"></script>
-        <script src="js/cufon-yui.js" type="text/javascript"></script>
-        <script src="js/cufon-replace.js" type="text/javascript"></script>
-        <script src="js/Open_Sans_400.font.js" type="text/javascript"></script>
-        <script src="js/Open_Sans_italic_400.font.js" type="text/javascript"></script> 
-        <script src="js/FF-cash.js" type="text/javascript"></script>	  
-        <script src="js/jquery.easing.1.3.js" type="text/javascript"></script> 
-        <script src="js/tms-0.3.js" type="text/javascript"></script>
-        <script src="js/tms_presets.js" type="text/javascript"></script>  
+        <script src="<?= base_url() . 'js/jquery-1.6.2.min.js';?>" type="text/javascript"></script>
+        <script src="<?= base_url() . 'js/cufon-yui.js';?>" type="text/javascript"></script>
+        <script src="<?= base_url() . 'js/cufon-replace.js';?>" type="text/javascript"></script>
+        <script src="<?= base_url() . 'js/Open_Sans_400.font.js';?>" type="text/javascript"></script>
+        <script src="<?= base_url() . 'js/Open_Sans_italic_400.font.js';?> type="text/javascript"></script> 
+        <script src="<?= base_url() . 'js/FF-cash.js';?>" type="text/javascript"></script>	  
+        <script src="<?= base_url() . 'js/jquery.easing.1.3.js';?>" type="text/javascript"></script> 
+        <script src="<?= base_url() . 'js/tms-0.3.js';?>" type="text/javascript"></script>
+        <script src="<?= base_url() . 'js/tms_presets.js';?>" type="text/javascript"></script>  
         <!--[if lt IE 7]>
                 <div style=' clear: both; text-align:center; position: relative;'>
                         <a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0"  alt="" /></a>
