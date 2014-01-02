d2806bac95232c3c04ec4707ce75b89a1c8eacbd
Fernando Rubilar Zepeda <frubilar@icci.cl>
2014-01-02 14:01:40 -0300

    DB

--- webinformatica/application/views/index_1.php
+++ webinformatica/application/views/index_1.php
@@ -1,4 +1,6 @@
+
 <!DOCTYPE html>
+
 <html lang="en">
     <head>
         <title></title>
@@ -74,14 +76,16 @@
                         <div class="container_12">
                             <div class="grid_12">
                                 <nav>
+                          
                                     <ul class="menu">
-                                        <li class="active">$this->load->view('index_1');Inicio</a><strong></strong></li>
+                                        <li class="active"><?php $this->load->view('index_1'); ; ?>Inicio</a><strong></strong></li>
                                         <li><a href="staff.html">Academicos</a><strong></strong></li>
                                         <li><a href="health.html">Estudiantes</a><strong></strong></li>
                                         <li><a href="advices.html">Laboral</a><strong></strong></li>
                                         <li><a href="news.html">Noticias</a><strong></strong></li>
                                         <li class="last"><a href="contacts.html">Contactos</a><strong></strong></li>
                                     </ul>
+                                                
                                 </nav>
                                 <div class="slider-wrapper">
                                     <div class="slider">
