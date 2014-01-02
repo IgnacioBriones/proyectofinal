400e349f4a09f8e4eb80c10b39ce05380a675857
Fernando Rubilar Zepeda <frubilar@icci.cl>
2014-01-02 00:18:46 -0300

    main

--- webinformatica/application/controllers/main.php
+++ webinformatica/application/controllers/main.php
@@ -1,36 +1,39 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
- 
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
 class Main extends CI_Controller {
- 
-    function __construct()
-    {
+
+    function __construct() {
         parent::__construct();
- 
+
         $this->load->database();
         $this->load->helper('url');
-        /* ------------------ */ 
- 
+        /* ------------------ */
+
         $this->load->library('grocery_CRUD');
- 
     }
- 
-    public function index()
-    {
-        echo "<h1>Welcome to the world of Codeigniter</h1>";//Just an example to ensure that we get into the function
+
+    public function index() {
+        echo "<h1>Welcome to the world of Codeigniter</h1>"; //Just an example to ensure that we get into the function
         die();
     }
-    public function usuario()
-    {
-        $this->grocery_crud->set_table('usuario');
-        $output = $this->grocery_crud->render();
- 
-        echo "<pre>";
-        print_r($output);
-        echo "</pre>";
-        die();
+
+    function just_an_example() {
+        $crud = new grocery_CRUD();
+
+        $crud->set_model('usuario_model');
+        $crud->set_table('usuario');
+        $crud->set_relation_n_n('rut', 'password');
+
+        $output = $crud->render();
+
+        $this->_example_output($output);
     }
+
 }
- 
+
 /* End of file main.php */
 /* Location: ./application/controllers/main.php */
  
