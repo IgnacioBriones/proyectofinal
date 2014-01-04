f0770a42463758ea81b00399583f0cfc29f269a2
Fernando Rubilar Zepeda <frubilar@icci.cl>
2014-01-04 02:59:40 -0300

    prueba view

--- webinformatica/application/views/main_template.php
+++ webinformatica/application/views/main_template.php
@@ -2,5 +2,4 @@
 	$this->load->view('plantillas/header'); 
 	$this->load->view('navegacion');
 	$this->load->view('plantillas/body');
-// 	$this->load->view($main_content);
  	$this->load->view('plantillas/footer'); 
--- webinformatica/application/views/plantillas/header.php
+++ webinformatica/application/views/plantillas/header.php
@@ -25,3 +25,88 @@
                 <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
         <![endif]-->
     </head>
+    <body id="page1">
+        <script type="text/javascript">
+            $(function() {
+                $('.slider')._TMS({
+                    duration: 800,
+                    easing: 'easeOutQuad',
+                    preset: 'diagonalFade',
+                    pagination: '.pagination',
+                    slideshow: 7000,
+                    banners: false,
+                    pauseOnHover: true
+                })
+            })
+        </script>
+        <!-- header -->
+        <header>
+            <div class="row-top">
+                <div class="main">
+                    <div class="container_12">
+                        <div class="wrapper">
+                            <div class="grid_9">
+                                <h1>
+                                    <a class="logo" href="index.html">family center</a>
+                                </h1>  
+                            </div>
+                            <div class="grid_3">
+                                <form id="search-form" method="post" enctype="multipart/form-data">
+                                    <fieldset>	
+                                        <div class="search-field">
+                                            <input name="search" type="text" value="Search" onFocus="if (this.value == 'Search') {
+                            this.value = ''
+                        }" onBlur="if (this.value == '') {
+                    this.value = 'Search'
+                }" />
+                                        </div>
+                                        <a class="search-button" href="#" onClick="document.getElementById('search-form').submit()"><span>search</span></a>							
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row-bot">
+                <div class="row-bot-shadow">
+                    <div class="main">
+                        <div class="container_12">
+                            <div class="grid_12">
+                                <nav>
+                                    <ul class="menu">
+                                        <li class="active"><a href="index.html">Inicio</a><strong></strong></li>
+                                        <li><a href="staff.html">Academicos</a><strong></strong></li>
+                                        <li><a href="health.html">Estudiantes</a><strong></strong></li>
+                                        <li><a href="advices.html">Laboral</a><strong></strong></li>
+                                        <li><a href="news.html">Noticias</a><strong></strong></li>
+                                        <li class="last"><a href="contacts.html">Contactos</a><strong></strong></li>
+                                    </ul>
+                                </nav>
+                                <div class="slider-wrapper">
+                                    <div class="slider">
+                                        <ul class="items">
+                                            <li>
+                                                <img src="images/slider-img1.jpg" alt="" />
+                                            </li>
+                                            <li>
+                                                <img src="images/slider-img2.jpg" alt="" />
+                                            </li>
+                                            <li>
+                                                <img src="images/slider-img3.jpg" alt="" />
+                                            </li>
+                                        </ul>
+                                        <ul class="pagination">
+                                            <li><a href="#">1</a></li>
+                                            <li><a href="#">2</a></li>
+                                            <li><a href="#">3</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header><div class="ic">More Website Templates  @ TemplateMonster.com - August 15th 2011!</div>
