8d633e6fc353f12863c958b592532b775c7c4d0f
fernando <frubilar@icci.cl>
2014-01-04 04:21:19 -0300

    ups

--- webinformatica/application/views/main_template.php
+++ webinformatica/application/views/main_template.php
@@ -2,6 +2,7 @@
 
     $this->load->view('plantillas/header');
     $this->load->view('navegacion');
-    $this->load->view('plantillas/body');
+//    $this->load->view('body_contactos.php');
+   $this->load->view('plantillas/body_contacto');
     $this->load->view('plantillas/footer');
 
